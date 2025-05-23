35fb2547d477beda9f252ab336c0aabeb77bbeed
Winarlyn <senya.belonogov@gmail.com>
2025-05-23 15:06:11 +0500

    for while

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-{
-    let a, b, mul;
-    a = 5;
-    b = 6;
-    mul = a * b;
-    console.log(mul);
+// {
+//     let a, b, mul;
+//     a = 5;
+//     b = 6;
+//     mul = a * b;
+//     console.log(mul);
     
-}
+// }
 
 {
     // let photosInFolder, photosOnPage, numberOfPage;
@@ -16,11 +16,11 @@
     // alert("кол-во страниц = " + numberOfPage);
 }
 
-{
-    let a = 1,b;
-    b = a++;
-    console.log(a,b);
-}
+// {
+//     let a = 1,b;
+//     b = a++;
+//     console.log(a,b);
+// }
 
 // {
 //     let a = 15 % 4;
@@ -42,25 +42,62 @@
 //     let x, y, max;
 //     x = parseFloat(prompt("Введите первое число"));
 //     y = parseFloat(prompt("Введите второе число"));
-//     if (x > y){
+//     z = parseFloat(prompt("Введите второе число"));
+//     if (x >= y && x >= z){
 //         max = x;
 //     }
-//     else {
+//     else  if(y >= x && y >= z){
 //         max = y;
+//     }
+//     else {
+//         max = z;
 //     }
 //     alert("max = " + max);
 
 // }
 
-{
-    //найти максимум из двух чисел 2 способ
-    let x, y, max;
-    x = parseFloat(prompt("Введите первое число"));
-    y = parseFloat(prompt("Введите второе число"));
-    max = x;
-    if (y > max){
-        max = y;
-    }
-    alert("max = " + max);
-}
+// {
+//     //найти максимум из двух чисел 2 способ
+//     let x, y, z, max;
+//     x = parseFloat(prompt("Введите первое число"));
+//     y = parseFloat(prompt("Введите второе число"));
+//     z = parseFloat(prompt("Введите второе число"));
+//     max = x;
+//     if (y > max){
+//         max = y;
+//     }
+//     if (z > max){
+//         max = z;
+//     }
+//     alert("max = " + max);
+// }
 
+// {
+//     // даны n наутральных чисел Найти максимум среди них
+//     let n, a, max;
+//     n = parseInt(prompt("Введите кол-во чисел"));
+//     max = parseInt(prompt("Введите число"));
+//     for(let i = 1; i < n; i++){
+//         a = parseInt(prompt("Введите число"));
+//         if (a > max){
+//             max = a;
+//         }
+//     }
+// alert("max = " + max);
+
+// }
+
+{
+    // дана последовательность наутральных чисел, которая заканчивается вводом числа 0. Найти максимум среди этой последовательности 
+    let a, max;
+    a = parseInt(prompt("Введите число"));
+    max = a;
+    while(a != 0){
+        a = parseInt(prompt("Введите число"));
+        if (a > max){
+            max = a;
+        }
+    }
+alert("max = " + max);
+
+}
